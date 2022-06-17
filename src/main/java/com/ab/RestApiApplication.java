fe97f07c18aca428614464ca4cddb20719205cc6
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-06-17 14:28:14 +0100

    pom updated

--- src/main/java/com/ab/RestApiApplication.java
+++ src/main/java/com/ab/RestApiApplication.java
@@ -2,8 +2,10 @@ package com.ab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component("com.ab.controllers")
 public class RestApiApplication {
 
 	public static void main(String[] args) {
